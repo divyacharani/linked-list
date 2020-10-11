863a38a62e63f1725e28292fe1a8d634e3bf18b4
unknown <divyacharaninabha@gmail.com>
2020-10-11 19:33:09 +0530

    Added: Functionality to create simple linked list added

--- src/com/bridgelabz/linkedlist/LinedListMain.java
+++ src/com/bridgelabz/linkedlist/LinedListMain.java
@@ -3,8 +3,8 @@ package com.bridgelabz.linkedlist;
 public class LinedListMain {
 
 	public static void main(String[] args) {
-		
-		//Welcome message
+
+		// Welcome message
 		System.out.println("Welcome to linked list program");
 	}
 }
