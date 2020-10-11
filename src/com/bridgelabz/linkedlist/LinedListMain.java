8c06fa552a2fa21173440f28d1bce1030c0ed8e2
unknown <divyacharaninabha@gmail.com>
2020-10-11 16:16:48 +0530

    Added: Welcome message added

---
+++ src/com/bridgelabz/linkedlist/LinedListMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linkedlist;
+
+public class LinedListMain {
+
+	public static void main(String[] args) {
+		
+		//Welcome message
+		System.out.println("Welcome to linked list program");
+	}
+}
